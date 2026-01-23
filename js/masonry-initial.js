96c17fde9f2f800e7ccf89e5498aa564cc4f4b7e
Korbin <klnevius@gmail.com>
2026-01-22 19:48:24 -0500

    fixed masonry problems and script placement

--- js/masonry-initial.js
+++ js/masonry-initial.js
@@ -1,12 +1,14 @@
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry
-var $grid = $('.grid').masonry({
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry(grid, {
     itemSelector: '.grid-item',
-    percentPosition: true,
-    columnWidth: '.grid-sizer'
+    columnWidth: '.grid-sizer',
+    percentPosition: true
 });
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-    $grid.masonry();
+
+imagesLoaded(grid).on('progress', function () {
+    msnry.layout();
 });
