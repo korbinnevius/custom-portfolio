a829e661b033c7b0edeb02a0ad7edd1e7ee82720
Korbin Nevius <klnevius@gmail.com>
2026-02-28 15:03:24 -0500

    mobile friendly test

--- js/cursor.js
+++ js/cursor.js
@@ -1,28 +1,32 @@
-let innerCursor = document.querySelector('.inner-cursor')
-let outerCursor = document.querySelector('.outer-cursor')
+if (window.matchMedia("(pointer: fine)").matches) {
 
-document.addEventListener('mousemove', mouseCursor)
+    let innerCursor = document.querySelector('.inner-cursor')
+    let outerCursor = document.querySelector('.outer-cursor')
 
-function mouseCursor(e) {
-    let x = e.clientX;
-    let y = e.clientY;
+    document.addEventListener('mousemove', mouseCursor)
 
-    // console.log(x, y)
+    function mouseCursor(e) {
+        let x = e.clientX;
+        let y = e.clientY;
 
-    innerCursor.style.left = `${x}px`;
-    innerCursor.style.top = `${y}px`;
-    outerCursor.style.left = `${x}px`;
-    outerCursor.style.top = `${y}px`;
+        // console.log(x, y)
+
+        innerCursor.style.left = `${x}px`;
+        innerCursor.style.top = `${y}px`;
+        outerCursor.style.left = `${x}px`;
+        outerCursor.style.top = `${y}px`;
+    }
+
+    let links = Array.from(document.querySelectorAll("a"));
+
+    console.log(links);
+
+    links.forEach(link => {
+        link.addEventListener("mouseover", ()=> innerCursor.classList.add("grow"));
+        link.addEventListener("mouseover", ()=> outerCursor.classList.add("rotate"));
+
+        link.addEventListener("mouseleave", ()=> innerCursor.classList.remove("grow"));
+        link.addEventListener("mouseleave", ()=> outerCursor.classList.remove("rotate"));
+    })
+
 }
-
-let links = Array.from(document.querySelectorAll("a"));
-
-console.log(links);
-
-links.forEach(link => {
-    link.addEventListener("mouseover", ()=> innerCursor.classList.add("grow"));
-    link.addEventListener("mouseover", ()=> outerCursor.classList.add("rotate"));
-
-    link.addEventListener("mouseleave", ()=> innerCursor.classList.remove("grow"));
-    link.addEventListener("mouseleave", ()=> outerCursor.classList.remove("rotate"));
-})
